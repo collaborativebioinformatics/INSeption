0240615203eee80bbfa24ac220ab9e03b1caedcc
mehelmy <medhat.helmy77@gmail.com>
2021-10-10 17:39:39 -0500

    Extract INS that can not be resloved by SV caller

--- analysis.sh
+++ analysis.sh
@@ -2,3 +2,9 @@
 Extract unaligned reads from bam file.
 """
 samtools view -f 4 my.bam | samtools fastq -@ 5  | bgzip > HG002.GRCh37.unmapped.fastq.gz
+
+
+"""
+Extract INS that can't be resolved by Sniffles 1.0.12.
+"""
+bcftools view -i 'INFO/SVTYPE="INS" & INFO/SVLEN=999' HG002.HiFi.GRCh37.SVLEN50.RE10.vcf > HG002.HiFi.GRCh37.SVLEN50.RE10.largeINS.vcf
