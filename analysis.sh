8f36c3f74da46ce354879bdf87c9054391758480
hoeps <wolfram.hoeps@embl.de>
2021-10-11 11:11:15 +0200

    Adding scripts and plots for tasks 2, 4 and 5

--- analysis.sh
+++ analysis.sh
@@ -8,3 +8,10 @@ samtools view -f 4 my.bam | samtools fastq -@ 5  | bgzip > HG002.GRCh37.unmapped
 Extract INS that can't be resolved by Sniffles 1.0.12.
 """
 bcftools view -i 'INFO/SVTYPE="INS" & INFO/SVLEN=999' HG002.HiFi.GRCh37.SVLEN50.RE10.vcf > HG002.HiFi.GRCh37.SVLEN50.RE10.largeINS.vcf
+
+
+"""
+Extract RE and SVTYPE, make plots for RE (task 2), SV type (task 4) and INS-RE (task 5)
+"""
+bcftools query -f "%INFO/RE\t%INFO/SVTYPE\n" HG002.HiFi.GRCh37.SVLEN50.RE10.vcf > RE_SVTYPE.tsv
+Rscript scripts/make_plots_RE_and_SVTYPE.R -i RE_SVTYPE.tsv -p plots/RE_dist_task2.pdf -q plots/SV_type_task4.pdf -r plots/INS_RE_dist_task5.pdf
---
+++ scripts/make_plots_RE_and_SVTYPE.R
@@ -0,0 +1,82 @@
+# Plot RE (Task #2)
+
+library(ggplot2)
+library(dplyr)
+library(optparse)
+
+# (Semi-) hardcoded parameters for plotting
+params <- vector(mode = "list")
+params$plotwidth = 5
+params$plotheight = 4
+params$plotunit = 'in'
+params$debug = F
+
+# Optparse to get user input ---------------------------------------------------------------
+if (!params$debug){
+option_list <- list(
+  make_option(c("-i", "--input_file"), type = "character", default = NULL,
+              help = "", metavar = "character"),
+  make_option(c("-p", "--output_plot1"), type = "character", default = NULL,
+              help = "", metavar = "character"),
+  make_option(c("-q", "--output_plot2"), type = "character", default = NULL,
+              help = "", metavar = "character"),
+  make_option(c("-r", "--output_plot3"), type = "character", default = NULL,
+              help = "", metavar = "character")
+)
+
+opt <- parse_args(OptionParser(option_list = option_list))
+
+infile_link    = opt$input_file 
+outplot1_link  = opt$output_plot1
+outplot2_link  = opt$output_plot2
+outplot3_link  = opt$output_plot3
+} else if (params$debug){
+  # For easy running in debug mode
+  infile_link = "../../RE_SVTYPE.tsv"
+  output_plot1 = "../plots/fooplot1.pdf"
+  output_plot2 = "../plots/fooplot2.pdf"
+  output_plot3 = "../plots/fooplot3.pdf"
+  
+}
+
+# Load files
+RElist = read.table(infile_link)
+colnames(RElist) = c('RE','SVTYPE')
+  
+# Count SV types for nicer plotting of plot #2
+RElist2 = RElist %>% 
+  count(SVTYPE) %>% 
+  mutate(perc = n / nrow(RElist))
+
+# First plot: Histogram of RE, all SVs
+p1 = ggplot(RElist) + 
+  geom_histogram(aes(x=RE, fill=SVTYPE), bins=100) +
+  scale_x_log10() + 
+  theme_bw() + 
+  labs(x='Reads supporting SV', y='#SVs', title = 'Reads supporting SV')
+
+# Second plot: Barplot of SVTYPES
+p2 = ggplot(RElist2) + 
+  geom_bar(aes(x=reorder(SVTYPE, -perc), y=n, fill=SVTYPE), stat='identity') + 
+  theme_bw() + 
+  theme(legend.position = 'none') + 
+  scale_y_log10() + 
+  labs(x='SV type', y='#SVs', title = 'Frequency of SV types')
+
+# Third plot: Histogram of RE, INS only
+p3 = ggplot(RElist[RElist$SVTYPE=='INS',]) + 
+  geom_histogram(aes(x=RE), bins=100, fill='#00B8E5') +
+  scale_x_log10() + 
+  theme_bw() + 
+  labs(x='Reads supporting Insertions', y='#INS', title = 'Reads supporting INS')
+
+# Save everything
+ggsave(p1, file=outplot1_link, device='pdf', width = params$plotwidth, 
+       height=params$plotheight, units=params$plotunit)
+ggsave(p2, file=outplot2_link, device='pdf', width = params$plotwidth, 
+       height=params$plotheight, units=params$plotunit)
+ggsave(p3, file=outplot3_link, device='pdf', width = params$plotwidth, 
+       height=params$plotheight, units=params$plotunit)
+
+# Inform user
+print('Output plots written.')
