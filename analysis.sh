291ba8b686cdaa10998ef53b8c7a6f88e1ad7b54
hoeps <wolfram.hoeps@embl.de>
2021-10-11 11:35:29 +0200

    Adding packages to requirements (am I doing this right?)

--- analysis.sh
+++ analysis.sh
@@ -13,5 +13,5 @@ bcftools view -i 'INFO/SVTYPE="INS" & INFO/SVLEN=999' HG002.HiFi.GRCh37.SVLEN50.
 """
 Extract RE and SVTYPE, make plots for RE (task 2), SV type (task 4) and INS-RE (task 5)
 """
-bcftools query -f "%INFO/RE\t%INFO/SVTYPE\n" HG002.HiFi.GRCh37.SVLEN50.RE10.vcf > RE_SVTYPE.tsv
-Rscript scripts/make_plots_RE_and_SVTYPE.R -i RE_SVTYPE.tsv -p plots/RE_dist_task2.pdf -q plots/SV_type_task4.pdf -r plots/INS_RE_dist_task5.pdf
+bcftools query -f "%INFO/RE\t%INFO/SVTYPE\n" HG002.HiFi.GRCh37.SVLEN50.RE10.vcf > HG002.Hifi.GRCh37.table_RE_SVTYPE.tsv
+Rscript scripts/make_plots_RE_and_SVTYPE.R -i HG002.Hifi.GRCh37.table_RE_SVTYPE.tsv -p plots/RE_dist_task2.pdf -q plots/SV_type_task4.pdf -r plots/INS_RE_dist_task5.pdf
