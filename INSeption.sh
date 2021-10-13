3fd46cd8f1c0f4171c1b4ed67a9c386042c7c360
David Molik <dave@d3fy.net>
2021-10-13 08:41:18 -1000

    basic skeleton of INSeption.sh

--- INSeption.sh
+++ INSeption.sh
@@ -1,37 +1,106 @@
-"""
-Extract unaligned reads from bam file.
-"""
+#!/bin/bash
+
+error () { printf "%b\n" "[$(date)]: \e[91m$*\e[0m" >&2; exit 1; }
+warn () { printf "%b\n" "[$(date)]: \e[93m$*\e[0m" >&2; }
+state () { printf "%b\n" "[$(date)]: $*" 2>&1; }
+describe () { printf "%b\n" "$*" 2>&1; } 
+pizzaz () { printf "%b\n" "\e[92m$*\e[0m" 2>&1; }
+collect () { printf "%b\n" "\e[96m$*\e[0m" 2>&1; read var; return $var; }
+
+stop_check() {
+  if [ -z "$force" ]; then
+    printf "%b\n" "\e[96m$* (y/n)?:\e[0m" 2>&1;
+    read cont
+    case "$cont" in
+      Y|y|yes|Yes|YES|YEs|yES|yEs|yeS )
+      ;;
+      N|n|no|No|NO|nO )
+        error "\"NO\" entered, exiting";
+      ;;
+      sure )
+        warn "sure, I mean, whatever..."
+        stop_check
+      ;;
+      *) 
+        warn "Invalid input You entered: $cont";
+	      stop_check
+      ;;
+    esac
+  fi
+}
+
+display_header(){
+  printf "%b\n" "\e[7;40m⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉\e[0m"
+  printf "%b\n" "\e[7;40m⨉⨉    ⨉⨉  ⨉⨉⨉⨉⨉⨉⨉  ⨉⨉⨉      ⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉\e[0m"
+  printf "%b\n" "\e[7;40m⨉⨉⨉  ⨉⨉⨉   ⨉⨉⨉⨉⨉⨉  ⨉⨉  ⨉⨉⨉⨉  ⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉\e[0m"
+  printf "%b\n" "\e[7;40m⨉⨉⨉  ⨉⨉⨉    ⨉⨉⨉⨉⨉  ⨉⨉  ⨉⨉⨉⨉  ⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉  ⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉\e[0m"
+  printf "%b\n" "\e[7;40m⨉⨉⨉  ⨉⨉⨉  ⨉⨉  ⨉⨉⨉  ⨉⨉⨉  ⨉⨉⨉⨉⨉⨉⨉⨉   ⨉⨉⨉    ⨉⨉⨉    ⨉⨉  ⨉⨉⨉   ⨉⨉⨉  ⨉ ⨉⨉⨉\e[0m"
+  printf "%b\n" "\e[7;40m⨉⨉⨉  ⨉⨉⨉  ⨉⨉⨉  ⨉⨉  ⨉⨉⨉⨉⨉  ⨉⨉⨉⨉⨉  ⨉  ⨉⨉  ⨉  ⨉⨉⨉  ⨉⨉⨉⨉⨉⨉⨉     ⨉⨉     ⨉⨉\e[0m"
+  printf "%b\n" "\e[7;40m⨉⨉⨉  ⨉⨉⨉  ⨉⨉⨉⨉  ⨉  ⨉⨉⨉⨉⨉⨉⨉  ⨉⨉⨉     ⨉⨉  ⨉  ⨉⨉⨉  ⨉⨉⨉  ⨉⨉  ⨉  ⨉⨉  ⨉  ⨉⨉\e[0m"
+  printf "%b\n" "\e[7;40m⨉⨉⨉  ⨉⨉⨉  ⨉⨉⨉⨉⨉    ⨉⨉  ⨉⨉⨉⨉  ⨉⨉  ⨉⨉⨉⨉⨉    ⨉⨉⨉⨉  ⨉⨉⨉  ⨉⨉  ⨉  ⨉⨉  ⨉  ⨉⨉\e[0m"
+  printf "%b\n" "\e[7;40m⨉⨉⨉  ⨉⨉⨉  ⨉⨉⨉⨉⨉⨉   ⨉⨉  ⨉⨉⨉⨉  ⨉⨉  ⨉  ⨉⨉  ⨉⨉⨉⨉⨉⨉  ⨉⨉⨉  ⨉⨉  ⨉  ⨉⨉  ⨉  ⨉⨉\e[0m"
+  printf "%b\n" "\e[7;40m⨉⨉    ⨉⨉  ⨉⨉⨉⨉⨉⨉⨉  ⨉⨉⨉      ⨉⨉⨉⨉   ⨉⨉⨉  ⨉⨉⨉⨉⨉⨉   ⨉⨉  ⨉⨉⨉   ⨉⨉⨉  ⨉  ⨉⨉\e[0m"
+  printf "%b\n" "\e[7;40m⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉⨉\e[0m"
+}
+
+help(){
+  display_header
+  describe "INSeption Version 0.1.0
+
+  utilities 
+    -h or --help
+       display this message and exit
+
+    -v or --version
+       display otb version and exit
+
+    -s or --supress
+       supress stop and checks, less verbose
+  "
+  exit 0;
+}
+
+version(){
+  describe "INSeption Version 0.1.0
+  polishing your mapping's structural variants with style.
+  created by:
+    Philippe Sanio, Wolfram Höps, Jedrzej Kubica, David Molik, Najeeb syed, and Medhat Mahmoud
+  "
+  exit 0;
+}
+
+while [ $# -gt 0 ] ; do
+  case $1 in
+    -h | --help) help ;;
+    -v | --version) version ;;
+    -s | --supress) SUPRESS="true";;
+  esac
+  shift
+done
+
+
+#  Extract unaligned reads from bam file.
 samtools view -f 4 my.bam | samtools fastq -@ 5  | bgzip > HG002.GRCh37.unmapped.fastq.gz
 
 
-"""
-Extract INS that can't be resolved by Sniffles 1.0.12.
-"""
+# Extract INS that can't be resolved by Sniffles 1.0.12.
 bcftools view -i 'INFO/SVTYPE="INS" & INFO/SVLEN=999' HG002.HiFi.GRCh37.SVLEN50.RE10.vcf > HG002.HiFi.GRCh37.SVLEN50.RE10.largeINS.vcf
 
 
-"""
-Extract RE and SVTYPE, make plots for RE (task 2), SV type (task 4) and INS-RE (task 5)
-"""
+# Extract RE and SVTYPE, make plots for RE (task 2), SV type (task 4) and INS-RE (task 5)
 bcftools query -f "%INFO/RE\t%INFO/SVTYPE\n" HG002.HiFi.GRCh37.SVLEN50.RE10.vcf > HG002.Hifi.GRCh37.table_RE_SVTYPE.tsv
 Rscript scripts/make_plots_RE_and_SVTYPE.R -i HG002.Hifi.GRCh37.table_RE_SVTYPE.tsv -p plots/RE_dist_task2.pdf -q plots/SV_type_task4.pdf -r plots/INS_RE_dist_task5.pdf
 
-"""
-Extract RE of long INS, make plots for RE (task 7)
-"""
+# Extract RE of long INS, make plots for RE (task 7)
 bcftool query -f "%INFO/RE\n" HG002.HiFi.GRCh37.SVLEN50.RE10.largeINS.vcf > HG002.Hifi.GRCh37.table_RE_largeINS.tsv
 Rscript scripts/make_plot_RE_dist.R -i ../HG002.Hifi.GRCh37.table_RE_largeINS.tsv -o plots/longIns_RE.pdf
 
-"""
-Make a plot showing the number of reads belonging to a cluster, as obtained by CARNAC-LR
-"""
+# Make a plot showing the number of reads belonging to a cluster, as obtained by CARNAC-LR
 wc -l *.fasta > n_seqs_per_cluster.txt
 sed -E 's/^[ ]*//g' n_seqs_per_cluster.txt > n_seqs_per_cluster_2.txt
 script scripts/clustering_stats.R -i n_fields.txt -o plots/carnac_cluster_stats.pdf
 
-"""
-Clustering unmapped reads with CARNAC-LR
-"""
+# Clustering unmapped reads with CARNAC-LR
 minimap2 HG002.GRCh37.unmapped.fastq HG002.GRCh37.unmapped.fastq -X > minimap_output.paf
 python3 CARNAC-LR/scripts/paf_to_CARNAC.py minimap_output.paf HG002.GRCh37.unmapped.fastq input_carnac.txt
 ulimit -s unlimited
@@ -39,58 +108,35 @@ ulimit -s unlimited
 sed 's/@/>@/g' <(awk 'NR%3!=0' <(awk 'NR%4!=0' HG002.GRCh37.unmapped.fastq)) > HG002.GRCh37.unmapped.fasta
 ./scripts/CARNAC_to_fasta HG002.GRCh37.unmapped.fasta output_file 2 # NOTE: please use fasta, do not ever use fastq
 
-"""
-Running assembly using flye flye/2.8.1
-"""
+# Running assembly using flye flye/2.8.1
 flye --pacbio-hifi HG002.GRCh37.unmapped.fastq.gz  --out-dir assembly --threads 32
 
-"""
-Running assembly using SPAdes/3.14.0
-"""
+# Running assembly using SPAdes/3.14.0
 spades.py -o spades_assembly  -s ../../HG002.GRCh37.unmapped.fastq.gz  --only-assembler
 
 
-"""
-Assemble cluster using flye
-"""
+# Assemble cluster using flye
 flye --pacbio-hifi  cluster62.fasta  --out-dir 62
 
-
-"""
-Get reade name for each SV (INS) and write it out in a file carrying the INS id
-"""
+# Get reade name for each SV (INS) and write it out in a file carrying the INS id
 bcftools query -f '%ID\t%INFO/RNAMES\n' HG002.HiFi.GRCh37.SVLEN50.RE10.largeINS.vcf |  while read a b ; do c=$(echo $b | tr ',' '\n'); echo $c > ./read_name_per_sv/"${a}".txt ; done
 
-"""
-Extract reads from BAM file
-"""
+# Extract reads from BAM file
 for i in $(ls $PWD/read_name_per_sv/*.txt); do a=$(basename $i); c=$(echo $a | cut -d'.' -f 1); samtools view my.bam | grep -f $i | cut -f 1,10 | awk '!/*/ {print ">"$1"\n"$2}' > "${c}".fasta
 
 
-"""
-Run spades (much faster than flye) on every cluster.fasta
-"""
+# Run spades (much faster than flye) on every cluster.fasta
 python3 scripts/clusterAssembler_general.py multi spades spades.py fastas_to_assemble/ assemblies/
 
-"""
-Get the contigs.fasta from all successful runs, collect them in one directory. 
-Then concatenate them to use as a pseudoreference for minimap2 in a later step.
-"""
+# Get the contigs.fasta from all successful runs, collect them in one directory. 
+# Then concatenate them to use as a pseudoreference for minimap2 in a later step.
 bash scripts/collect_assembly_fastas.sh assemblies spades_contigs_fasta
 cat spades_contigs_fasta/*.fasta > spades_contigs_fasta/spades_pseudoreference.fasta
 samtools faidx spades_contigs_fasta/spades_pseudoreference.fasta
 
-"""
-Merge ins supporting reads, also as input to minimap
-"""
+# Merge ins supporting reads, also as input to minimap
 cat fasta_support_ins/*.fasta > fasta_support_ins/all.fasta
 
-"""
-Run minimap2: align all reads supporting ins against the contigs. '-P' parameter to retain all alignments, even secondary ones. 
-"""
+# Run minimap2: align all reads supporting ins against the contigs. '-P' parameter to retain all alignments, even secondary ones. 
 minimap2 -x map-hifi spades_contigs_fasta/spades_pseudoreference.fasta fasta_support_ins/all.fasta -a -P > ins_reads_vs_contigs.sam
 samtools sort -O sam -o ins_reads_vs_contigs.sort.sam ins_reads_vs_contigs.sam
-
-
-
-
--- scripts/clustering_stats.R
+++ scripts/clustering_stats.R
@@ -1,51 +1,5 @@
-# Clustering overview
+Script started on Wed Oct 13 08:39:24 2021
+command: -i n_fields.txt -o plots/carnac_cluster_stats.pdf
+script: -i: No such file or directory
 
-##### A) LOAD PACKAGES AND SET PARAMETERS ########
-library(ggplot2)
-library(optparse)
-
-# (Semi-) hardcoded parameters for plotting
-params <- vector(mode = "list")
-params$plotwidth = 7
-params$plotheight = 5
-params$plotunit = 'in'
-params$debug = F
-
-##### B) DIGEST USER INPUT WITH OPTPARSE#######
-if (!params$debug){
-  option_list <- list(
-    make_option(c("-i", "--input_file"), type = "character", default = NULL,
-                help = "", metavar = "character"),
-    make_option(c("-o", "--output_plot"), type = "character", default = NULL,
-                help = "", metavar = "character")
-  )
-
-
-opt <- parse_args(OptionParser(option_list = option_list))
-
-infile_link    = opt$input_file 
-outplot_link  = opt$output_plot
-} else if (params$debug){
-  # For easy running in debug mode
-  infile_link = "../data/n_fields.txt"
-  infile_link = "../data/clusters_metrics.txt"
-  infile_link = "../data/n_seqs_per_cluster_2.txt"
-  output_plot = "../plots/fooplot1.pdf"
-}
-
-
-clusters = read.table(infile_link, sep=' ', header=F)
-clusters$size = clusters$V1 / 4
-p = ggplot(clusters) + geom_point(aes(x=1:length(size), y=sort(size, decreasing=F))) + 
-  geom_line(aes(x=1:length(size), y=sort(size, decreasing=F))) + 
-  theme_bw() + 
-  scale_y_log10(breaks=c(1,2,10,100,1000),limits=c(1,max(clusters$size))) +
-  labs(x='# Cluster', y='Reads belonging to cluster', 
-       title= paste0('CARNAC-LR: Reads and clusters \nSum or reads: ',
-                     sum(clusters$size),'\nSingletons: ', sum(clusters$size==1),
-                     ' clusters. >= 2 reads: ', sum(clusters$size>1), 
-                     ' clusters')) + 
-  scale_x_continuous(limits=c(1,length(clusters$size)))
-
-ggsave(p, file=outplot_link, device='pdf', width = params$plotwidth, 
-       height=params$plotheight, units=params$plotunit)
+Script done on Wed Oct 13 08:39:24 2021
